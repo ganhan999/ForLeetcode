ffc4a6e6868fbf03f77e7c7df990f6c9b6a1732d
ganhan99 <940320304@qq.com>
2020-11-12 22:38:59 +0800

    61、加一!

--- text.py
+++ text.py
@@ -1,20 +1,2 @@
-# from typing import List
-#
-#
-# class Solution:
-#     def plusOne(self, digits: List[int]) -> List[int]:
-#         for i in range(len(digits)-1,-1,-1):
-#             if digits[i]!=9:
-#                 digits[i] += 1
-#                 return digits
-#             else:
-#                 digits[i] =0
-#         digits.insert(0,1)
-#         return digits
-#
-#
-# a=Solution()
-# print(a.plusOne([0,9,9]))
-
-a=[1,2,3]
-print('-'.join(a))
+a=Solution()
+print(a.)
