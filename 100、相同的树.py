0242a07872b0c62bd0932aa62a6a2c23e6902637
ganhan99 <940320304@qq.com>
2020-11-18 22:17:28 +0800

    100、相同的树

---
+++ 100、相同的树.py
@@ -0,0 +1,77 @@
+"""
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+
+
+示例 2:
+输入:      1          1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+输出: false
+
+
+示例 3:
+输入:       1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+输出: false
+
+
+
+"""
+
+"""
+思路分析：
+递归大法好。
+一句话, 先想出口：
+    空的话怎么办？
+    一个出口怎么办？
+    通常情况下怎么处理当前元素, 同时减少问题规模？
+
+"""
+#我的做法
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q: return True
+        if not p or not q: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+#大神做法1
+
+"""
+深度优先遍历，把遍历的数字加入数组中。
+"""
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        resp, resq = [], []
+        self.DFS(p, resp)
+        self.DFS(q, resq)
+        return resp == resq
+
+    def DFS(self, tree, res):
+        if not tree:
+            res.append('')
+            return
+        res.append(tree.val)
+        self.DFS(tree.left, res)
+        self.DFS(tree.right, res)
+
+
--- text.py
+++ text.py
@@ -1,35 +1,20 @@
 from typing import List
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
 
-# 输入：
-# nums1 = [1,2,3,0,0,0], m = 3
-# nums2 = [2,5,6],       n = 3
-# 输出：[1,2,2,3,5,6]
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        while n:
-            if m == 0:
-                nums1[n-1] = nums2[n-1]
-                n -= 1
-                continue
-            if nums1[m-1] < nums2[n-1]:
-                nums1[m+n-1] = nums2[n-1]
-                n -= 1
-            else:
-                nums1[m+n-1] = nums1[m-1]
-                m -= 1
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# class Solution:
+#     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+#
+#
+#
+# a=Solution()
+#
+# a.merge([1,2,5,0,0,0,0],3,[4,5,6],3)
 
-
-
-
-a=Solution()
-
-a.merge([1,2,5,0,0,0,0],3,[4,5,6],3)
-
+l=[]
+print(not l)
