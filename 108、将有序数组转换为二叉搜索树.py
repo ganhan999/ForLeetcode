871cea60d45ea26e40986be99e8e90b852e12aa2
ganhan99 <940320304@qq.com>
2020-11-22 21:41:16 +0800

    108、将有序数组转换为二叉搜索树

---
+++ 108、将有序数组转换为二叉搜索树.py
@@ -0,0 +1,41 @@
+"""
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+给定有序数组: [-10,-3,0,5,9],
+
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+
+"""
+
+"""
+思路分析：
+DFS，递归，用二分的思想。
+"""
+#我的做法
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        maxlength=len(nums)
+        def DFS(left,right):
+            if left>right:
+                return
+            mid=(left+right)//2
+            root=TreeNode(nums[mid])
+            root.left=DFS(left,mid-1)
+            root.right=DFS(mid+1, right)
+            return root
+        return DFS(0, maxlength-1)
+
+
+
+
+
--- text.py
+++ text.py
@@ -2,33 +2,14 @@ from typing import List
 from collections import deque
 # import math
 
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 class Solution:
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
-        if not root:
-            return []
-        que=deque([root])
-        l=[]
-        while que:
-            nextque=deque()
-            l1=[]
-            while que:
-                num=que.popleft()
-                l1.append(num.val)
-                print (l1)
-                if num.left:
-                    nextque.append(num.left)
-                if num.right:
-                    nextque.append(num.right)
-            l.append(l1)
-            que=nextque
-        l=l[::-1]
-        return l
-l=[1,[1,2],[1,2,3]]
-l.reverse()
-print(l)
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def dfs(left, right):
+            if left > right:
+                return None
+            mid = (left+right) // 2
+            root = TreeNode(nums[mid])
+            root.left = dfs(left, mid-1)
+            root.right = dfs(mid+1, right)
+            return root
+        return dfs(0, len(nums)-1)
