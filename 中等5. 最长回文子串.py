92086c64695079a8335a6f91920f0ca51f2e9d73
ganhan99 <940320304@qq.com>
2020-12-11 16:55:33 +0800

    5

---
+++ 中等5. 最长回文子串.py
@@ -0,0 +1,126 @@
+"""
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+
+示例 2：
+输入: "cbbd"
+输出: "bb"
+
+
+"""
+
+"""
+动态规划，如果bab是回文，俺么ababa也是，把整个矩阵都填满，首先是对角线，然后慢慢扩散。
+"""
+#我的做法
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 l+1
+        for l in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+
+
+#大神做法1
+
+"""
+根据回文子串的定义，枚举所有长度大于等于 22 的子串，依次判断它们是否是回文；
+在具体实现时，可以只针对大于“当前得到的最长回文子串长度”的子串进行“回文验证”；
+在记录最长回文子串的时候，可以只记录“当前子串的起始位置”和“子串长度”，不必做截取。
+"""
+
+class Solution:
+    # 暴力匹配（超时）
+    def longestPalindrome(self, s: str) -> str:
+        # 特判
+        size = len(s)
+        if size < 2:
+            return s
+
+        max_len = 1
+        res = s[0]
+
+        # 枚举所有长度大于等于 2 的子串
+        for i in range(size - 1):
+            for j in range(i + 1, size):
+                if j - i + 1 > max_len and self.__valid(s, i, j):
+                    max_len = j - i + 1
+                    res = s[i:j + 1]
+        return res
+
+    def __valid(self, s, left, right):
+        # 验证子串 s[left, right] 是否为回文串
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+#大神做法2
+
+"""
+中心扩散法
+
+我们可以设计一个方法，兼容以上两种情况：
+
+1、如果传入重合的索引编码，进行中心扩散，此时得到的回文子串的长度是奇数；
+
+2、如果传入相邻的索引编码，进行中心扩散，此时得到的回文子串的长度是偶数。
+
+
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        # 至少是 1
+        max_len = 1
+        res = s[0]
+
+        for i in range(size):
+            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
+            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
+
+            # 当前找到的最长回文子串
+            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            if len(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+
+        return res
+
+    def __center_spread(self, s, size, left, right):
+        """
+        left = right 的时候，此时回文中心是一个字符，回文串的长度是奇数
+        right = left + 1 的时候，此时回文中心是一个空隙，回文串的长度是偶数
+        """
+        i = left
+        j = right
+
+        while i >= 0 and j < size and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return s[i + 1:j], j - i - 1
