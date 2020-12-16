d7fa08cfbb4c42d3acdaf81d93cb7eecaacdef1f
ganhan99 <940320304@qq.com>
2020-12-16 20:15:08 +0800

    15

--- 中等15. 三数之和.py
+++ 中等15. 三数之和.py
@@ -1,8 +1,24 @@
-from typing import List
-from collections import deque
-# import math
-import codecs
+"""
+给你一个包含 n 个整数的数组 nums，
+判断 nums 中是否存在三个元素 a，b，c ，
+使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+注意：答案中不可以包含重复的三元组。
 
+示例：
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+
+"""
+
+"""
+排序＋双指针法，排序后可以化简为双指针的twosum题   
+"""
+#我的做法
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
@@ -14,8 +30,7 @@ class Solution:
         for i in range(n):
             if (nums[i] > 0):
                 return res
-            print(nums)
-            if nums[i] == nums[i - 1]:
+            if (i > 0 and nums[i] == nums[i - 1]):#加i>0是为了避免[0,0,0,0]的情况
                 continue
             L = i + 1
             R = n - 1
@@ -35,5 +50,3 @@ class Solution:
         return res
 
 
-print(Solution().threeSum(
-[0,0,0]))
