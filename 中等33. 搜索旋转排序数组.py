ace8644b755f6111b51e68a1e9582f9b7b5b1e5f
ganhan99 <940320304@qq.com>
2020-12-29 20:39:42 +0800

    34

---
+++ 中等33. 搜索旋转排序数组.py
@@ -0,0 +1,52 @@
+"""
+升序排列的整数数组 nums 在预先未知的某个点上进行了旋转（例如， [0,1,2,4,5,6,7] 经旋转后可能变为 [4,5,6,7,0,1,2] ）。
+请你在数组中搜索 target ，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+示例 1：
+输入：nums = [4,5,6,7,0,1,2], target = 0
+输出：4
+
+示例 2：
+输入：nums = [4,5,6,7,0,1,2], target = 3
+输出：-1
+
+示例 3：
+输入：nums = [1], target = 0
+输出：-1
+
+
+"""
+
+"""
+题目要求算法时间复杂度必须是 O(\log n)O(logn) 的级别，这提示我们可以使用二分搜索的方法。
+
+但是数组本身不是有序的，进行旋转后只保证了数组的局部是有序的，这还能进行二分搜索吗？答案是可以的。
+
+可以发现的是，我们将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。
+拿示例来看，我们从 6 这个位置分开以后数组变成了 [4, 5, 6] 和 [7, 0, 1, 2] 两个部分，
+其中左边 [4, 5, 6] 这个部分的数组是有序的，其他也是如此。
+
+"""
+
+#大神做法1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
--- 中等34. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 中等34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,8 +1,33 @@
-from typing import List
-from collections import deque
-# import math
+"""
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+进阶：
+你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ 
+
+示例 1：
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+
+示例 2：
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+
+示例 3：
+输入：nums = [], target = 0
+输出：[-1,-1]
 
 
+
+"""
+
+"""
+二分查找，先找左边的数，如果mid等于target那么右边的target不可能是最左边的数。反之亦然。
+
+"""
+
+#大神做法1
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         size = len(nums)
@@ -36,7 +61,7 @@ class Solution:
         left = 0
         right = size - 1
         while left < right:
-            mid = (left + right + 1) // 2
+            mid = (left + right + 1) // 2   #这里是因为如果存在8，8的情况，那么需要去上整数才可以得到最右边的数
             if nums[mid] > target:
                 right = mid - 1
             elif nums[mid] == target:
@@ -44,8 +69,6 @@ class Solution:
             else:
                 # nums[mid] < target
                 left = mid + 1
-        # 由于能走到这里，说明在数组中一定找得到目标元素，因此这里不用再做一次判断
+        # 由于能走到这里，说明在数组中一定找得到目标元素，因此这里不用再做一次判断  因为左边已经找到了数
         return left
 
-
-print(Solution().searchRange([5,7,7,8,8,10],8))
