8c422520187731d2a6d9dcb3a070d2e62a077164
ganhan99 <940320304@qq.com>
2020-12-31 19:30:09 +0800

    39

---
+++ 中等39. 组合总和.py
@@ -0,0 +1,100 @@
+"""
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。 
+
+示例 1：
+输入：candidates = [2,3,6,7], target = 7,
+所求解集为：
+[
+  [7],
+  [2,2,3]
+]
+
+示例 2：
+输入：candidates = [2,3,5], target = 8,
+所求解集为：
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+ 
+提示：
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+candidate 中的每个元素都是独一无二的。
+1 <= target <= 500
+
+"""
+
+"""
+回溯法 
+
+以 target = 7 为 根结点 ，创建一个分支的时 做减法 ；
+每一个箭头表示：从父亲结点的数值减去边上的数值，得到孩子结点的数值。
+边的值就是题目中给出的 candidate 数组的每个元素的值；
+减到 0 或者负数的时候停止，即：结点 0 和负数结点成为叶子结点；
+所有从根结点到结点 0 的路径（只能从上往下，没有回路）就是题目要找的一个结果。
+
+"""
+
+#大神做法1
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def dfs(candidates, begin, size, path, res, target):
+            if target < 0:
+                return
+            if target == 0:
+                res.append(path)
+                return
+
+            for index in range(begin, size):
+                dfs(candidates, index, size, path + [candidates[index]], res, target - candidates[index])
+
+        size = len(candidates)
+        if size == 0:
+            return []
+        path = []
+        res = []
+        dfs(candidates, 0, size, path, res, target)
+        return res
+
+
+
+"""
+剪枝提速
+
+根据上面画树形图的经验，如果 target 减去一个数得到负数，那么减去一个更大的树依然是负数，同样搜索不到结果。基于这个想法，我们可以对输入数组进行排序，添加相关逻辑达到进一步剪枝的目的；
+排序是为了提高搜索速度，对于解决这个问题来说非必要。但是搜索问题一般复杂度较高，能剪枝就尽量剪枝。实际工作中如果遇到两种方案拿捏不准的情况，都试一下。
+
+"""
+
+#大神做法2
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def dfs(candidates, begin, size, path, res, target):
+            if target == 0:
+                res.append(path)
+                return
+
+            for index in range(begin, size):
+                residue = target - candidates[index]
+                if residue < 0:#在循环内判断是否要剪枝
+                    break
+
+                dfs(candidates, index, size, path + [candidates[index]], res, residue)
+
+        size = len(candidates)
+        if size == 0:
+            return []
+        candidates.sort()
+        path = []
+        res = []
+        dfs(candidates, 0, size, path, res, target)
+        return res
--- 中等text！.py
+++ 中等text！.py
@@ -4,19 +4,25 @@ from collections import deque
 
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = [{} for _ in range(9)]
-        col = [{} for _ in range(9)]
-        grid = [[{} for _ in range(3)] for _ in range(3)]
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    tmp = int(board[i][j])
-                    row[i][tmp] = row[i].get(tmp, 0) + 1#如果已经存在该数字，那么就继续加一，没有就从0开始
-                    col[j][tmp] = col[j].get(tmp, 0) + 1
-                    grid[i//3][j//3][tmp] = grid[i//3][j//3].get(tmp, 0) + 1
-                    if row[i].get(tmp) > 1 or col[j].get(tmp) > 1 or grid[i//3][j//3].get(tmp) > 1:#如果对应的value大于1说明，存在两次，直接退出循环
-                        return False
-        return True
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(candidates, begin, size, path, res, target):
+            if target < 0:
+                return
+            if target == 0:
+                res.append(path)
+                return
 
-print(Solution().isValidSudoku([["5","3","3",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]))
+            for index in range(begin, size):
+                dfs(candidates, index, size, path + [candidates[index]], res, target - candidates[index])
+
+        size = len(candidates)
+        if size == 0:
+            return []
+        path = []
+        res = []
+        dfs(candidates, 0, size, path, res, target)
+        return res
+
+
+
+print(Solution().combinationSum([2,3,6,7],7))
