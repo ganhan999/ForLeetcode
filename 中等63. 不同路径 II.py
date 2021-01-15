78bcec14016dbd7e04c4c68915b7ebc60d0c0d84
ganhan99 <940320304@qq.com>
2021-01-15 20:57:14 +0800

    63

---
+++ 中等63. 不同路径 II.py
@@ -0,0 +1,44 @@
+"""
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+示例1：
+输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+输出：2
+解释：
+3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+
+
+示例2：
+输入：obstacleGrid = [[0,1],[0,0]]
+输出：1
+"""
+
+
+"""
+动态规划，每一个格子都是左边或者上面格子的总和，如果遇到障碍物，那么就默认设置为0，只有另外一个方向可以走了。
+"""
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        #新建矩阵版
+        height, width = len(obstacleGrid),len(obstacleGrid[0])
+        store = [[0]*width for i in range(height)]
+
+        #从上到下，从左到右
+        for m in range(height):#每一行
+            for n in range(width):#每一列
+                if not obstacleGrid[m][n]: #如果这一格没有障碍物,如果有那么就把它默认为0
+                    if m == n == 0: #或if not(m or n)
+                        store[m][n] = 1
+                    else:
+                        a = store[m-1][n] if m!=0 else 0 #上方格子
+                        b = store[m][n-1] if n!=0 else 0 #左方格子
+                        store[m][n] = a+b
+        return store[-1][-1]
+
+
