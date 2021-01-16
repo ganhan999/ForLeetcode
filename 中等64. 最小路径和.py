25e5c2a3e047d17545814476865b9ba6293b740d
ganhan99 <940320304@qq.com>
2021-01-16 19:21:01 +0800

    64

--- 中等64. 最小路径和.py
+++ 中等64. 最小路径和.py
@@ -1,14 +1,23 @@
-import collections
-from typing import List
-from collections import deque
-# import math
+"""
+给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+说明：每次只能向下或者向右移动一步。
+
+ 
+示例 1：
+输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+输出：7
+解释：因为路径 1→3→1→1→1 的总和最小。
+
+示例 2：
+输入：grid = [[1,2,3],[4,5,6]]
+输出：12
+
+"""
 
 
-from typing import List
-
-from typing import List
-
-
+"""
+动态规划，先把最上边和最左边的最短路径算出来，因为最上和最左只有唯一的路径可以走。
+"""
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
@@ -25,6 +34,3 @@ class Solution:
             for j in range(1, columns):
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
         return dp[rows - 1][columns - 1]
-
-
-print(Solution().minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
