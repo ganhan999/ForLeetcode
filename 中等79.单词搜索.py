80de3c3598a2e1d952065ed0996ddd2ff5e6fd48
ganhan99 <940320304@qq.com>
2021-01-27 19:22:09 +0800

    79

--- 中等79.单词搜索.py
+++ 中等79.单词搜索.py
@@ -1,11 +1,35 @@
-import collections
-from typing import List
-from collections import deque
-# import math
-from typing import List
-import itertools
+"""
+给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+同一个单元格内的字母不允许被重复使用。
+
+示例:
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+给定 word = "ABCCED", 返回 true
+给定 word = "SEE", 返回 true
+给定 word = "ABCB", 返回 false
+ 
+提示：
+board 和 word 中只包含大写和小写英文字母。
+1 <= board.length <= 200
+1 <= board[i].length <= 200
+1 <= word.length <= 10^3
+通过次数134,541提交次数305,651
 
 
+"""
+
+
+"""
+回溯
+"""
+#大神做法1
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
@@ -14,6 +38,12 @@ class Solution:
         rows = [-1, 0, 1, 0]
         cols = [0, 1, 0, -1]
         def dfs(x, y, idx):
+            """搜索单词
+            Args:
+                x: 行索引
+                y: 列索引
+                idx: 单词对应的字母索引
+            """
             if board[x][y] != word[idx]:
                 return False
             if idx == len(word) - 1:
@@ -35,4 +65,5 @@ class Solution:
                 if dfs(x, y, 0):
                     return True
         return False
-print(Solution().exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"))
+
+
