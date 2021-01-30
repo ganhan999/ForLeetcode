6020ac582c440a41f2019d99828bfcd587872fee
ganhan99 <940320304@qq.com>
2021-01-30 12:54:22 +0800

    82

--- 中等82. 删除排序链表中的重复元素 II.py
+++ 中等82. 删除排序链表中的重复元素 II.py
@@ -1,15 +1,24 @@
-import collections
-from typing import List
-from collections import deque
-# import math
-from typing import List
-import itertools
+"""
+给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
 
-class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+示例 1:
+输入: 1->2->3->3->4->4->5
+输出: 1->2->5
 
+示例 2:
+输入: 1->1->1->2->3
+输出: 2->3
+
+
+"""
+
+
+
+"""
+直接按照规律循环解题。
+但避免开头就出现重复数字，因此还是先加空头
+"""
+#大神做法1
 class Solution:#感谢各位的更好思路或改进办法
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         thead = ListNode('a')
@@ -24,6 +33,3 @@ class Solution:#感谢各位的更好思路或改进办法
                     cur=cur.next
             pre.next=cur
         return thead.next
-
-
-print(Solution().deleteDuplicates([1,2,3,3,4,4,5]))
