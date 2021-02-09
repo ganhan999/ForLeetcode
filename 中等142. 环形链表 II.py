034a01dda05ad4a0824ff42927a202257c7ef8a9
ganhan99 <940320304@qq.com>
2021-02-09 20:17:49 +0800

    142

---
+++ 中等142. 环形链表 II.py
@@ -0,0 +1,78 @@
+"""
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+说明：不允许修改给定的链表。
+
+进阶：
+
+你是否可以使用 O(1) 空间解决此题？
+ 
+示例 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：返回索引为 1 的链表节点
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例 2：
+输入：head = [1,2], pos = 0
+输出：返回索引为 0 的链表节点
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+输入：head = [1], pos = -1
+输出：返回 null
+解释：链表中没有环。
+
+
+
+"""
+
+
+"""
+快慢指针法
+因此，当发现 slow 与 fast 相遇时，我们再额外使用一个指针 ptr。起始，它指向链表头部；
+随后，它和slow每次向后移动一个位置。最终，它们会在入环点相遇。
+
+
+"""
+#大神做法1
+class Solution(object):
+    def detectCycle(self, head):
+        fast, slow = head, head
+        while True:
+            if not (fast and fast.next): return
+            fast, slow = fast.next.next, slow.next
+            if fast == slow: break
+        fast = head
+        while fast != slow:
+            fast, slow = fast.next, slow.next
+        return fast
+
+
+
+
+
+"""
+建立set集合
+"""
+#大神做法2
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        seen = set()
+        while head:
+            # 若存在，则表示存在环，返回True
+            if head in seen:
+                return True
+
+            # 将节点加入seen
+            seen.add(head)
+            # 移向下一个节点
+            head = head.next
+        return None
+
+
