7e731a07d5dd280ebc704c58f7af2e4bb6d9aa7a
ganhan99 <940320304@qq.com>
2021-02-11 13:06:16 +0800

    148

---
+++ 中等146. LRU 缓存机制.py
@@ -0,0 +1,115 @@
+"""
+运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+实现 LRUCache 类：
+
+LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，
+则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ 
+进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
+
+ 
+示例：
+输入
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+
+输出
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+解释
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // 缓存是 {1=1}
+lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+lRUCache.get(1);    // 返回 1
+lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+lRUCache.get(2);    // 返回 -1 (未找到)
+lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+lRUCache.get(1);    // 返回 -1 (未找到)
+lRUCache.get(3);    // 返回 3
+lRUCache.get(4);    // 返回 4
+
+
+"""
+
+
+"""
+首先建立key：value键值对，把键值对放入hashmap中，也就是python的字典。
+然后再用双向链表的方式进行操作，链表中存储的是hashmap中的value
+"""
+#大神做法1
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        # 新建两个节点 head 和 tail
+        self.head = ListNode()
+        self.tail = ListNode()
+        # 初始化链表为 head <-> tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # 因为get与put操作都可能需要将双向链表中的某个节点移到末尾，所以定义一个方法
+    def move_node_to_tail(self, key):
+            # 先将哈希表key指向的节点拎出来，为了简洁起名node
+            #      hashmap[key]                               hashmap[key]
+            #           |                                          |
+            #           V              -->                         V
+            # prev <-> node <-> next         pre <-> next   ...   node
+            node = self.hashmap[key]
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            # 之后将node插入到尾节点前
+            #                 hashmap[key]                 hashmap[key]
+            #                      |                            |
+            #                      V        -->                 V
+            # prev <-> tail  ...  node                prev <-> node <-> tail
+            node.prev = self.tail.prev
+            node.next = self.tail
+            self.tail.prev.next = node
+            self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            # 如果已经在链表中了久把它移到末尾（变成最新访问的）
+            self.move_node_to_tail(key)
+        res = self.hashmap.get(key, -1)
+        if res == -1:
+            return res
+        else:
+            return res.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            # 如果key本身已经在哈希表中了就不需要在链表中加入新的节点
+            # 但是需要更新字典该值对应节点的value
+            self.hashmap[key].value = value
+            # 之后将该节点移到末尾
+            self.move_node_to_tail(key)
+        else:
+            if len(self.hashmap) == self.capacity:
+                # 去掉哈希表对应项
+                self.hashmap.pop(self.head.next.key)
+                # 去掉最久没有被访问过的节点，即头节点之后的节点
+                self.head.next = self.head.next.next
+                self.head.next.prev = self.head
+            # 如果不在的话就插入到尾节点前
+            new = ListNode(key, value)
+            self.hashmap[key] = new
+            new.prev = self.tail.prev
+            new.next = self.tail
+            self.tail.prev.next = new
+            self.tail.prev = new
+
+
+
+
---
+++ 中等148. 排序链表.py
@@ -0,0 +1,129 @@
+"""
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+
+进阶：
+
+你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ 
+
+示例 1：
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
+
+示例 2：
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
+
+示例 3：
+输入：head = []
+输出：[]
+
+
+"""
+
+
+"""
+合并排序，利用递归，自上而下
+"""
+#大神做法1
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def sortFunc(head: ListNode, tail: ListNode) -> ListNode:#用快慢指针找到中点，分成两个链表
+            if not head:
+                return head
+            if head.next == tail:
+                head.next = None
+                return head
+            slow = fast = head
+            while fast != tail:
+                slow = slow.next
+                fast = fast.next
+                if fast != tail:
+                    fast = fast.next
+            mid = slow
+            return merge(sortFunc(head, mid), sortFunc(mid, tail))
+
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+        return sortFunc(head, None)#一开始是以None作为fast的最后一个节点的判断条件
+
+
+"""
+合并排序，自底向上，先1后2再4以此类推
+"""
+#大神做法1
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+
+        if not head:
+            return head
+
+        length = 0
+        node = head
+        while node:
+            length += 1
+            node = node.next
+
+        dummyHead = ListNode(0, head)
+        subLength = 1
+        while subLength < length:
+            prev, curr = dummyHead, dummyHead.next
+            while curr:
+                head1 = curr
+                for i in range(1, subLength):
+                    if curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                head2 = curr.next
+                curr.next = None
+                curr = head2
+                for i in range(1, subLength):
+                    if curr and curr.next:
+                        curr = curr.next
+                    else:
+                        break
+
+                succ = None
+                if curr:
+                    succ = curr.next
+                    curr.next = None
+
+                merged = merge(head1, head2)
+                prev.next = merged
+                while prev.next:
+                    prev = prev.next
+                curr = succ
+            subLength <<= 1
+        return dummyHead.next
+
