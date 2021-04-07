09c360a7691f5f8fad5c9d3de09a74446eb5094a
ganhan <940320304@qq.com>
2021-04-07 23:45:58 +0800

    572

---
+++ 572、另一个树的子树.py
@@ -0,0 +1,87 @@
+"""
+给定两个非空二叉树 s 和 t，检验s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。
+s 也可以看做它自身的一棵子树。
+
+示例 1:
+给定的树 s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+
+
+示例 2:
+给定的树 s：
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 false。
+
+
+
+
+
+"""
+
+
+"""
+判断两个树是否相等的三个条件是与的关系，即：
+
+当前两个树的根节点值相等；
+并且，s 的左子树和 t 的左子树相等；
+并且，s 的右子树和 t 的右子树相等。
+而判断 t 是否为 s 的子树的三个条件是或的关系，即：
+
+当前两棵树相等；
+或者，t 是 s 的左子树；
+或者，t 是 s 的右子树。
+
+
+"""
+#大神做法1
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+
+
