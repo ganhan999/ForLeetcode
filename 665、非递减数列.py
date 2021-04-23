a50e1408b436292c323b27315112b09d18af4bac
ganhan <940320304@qq.com>
2021-04-23 19:55:13 +0800

    671

--- 665、非递减数列.py
+++ 665、非递减数列.py
@@ -21,21 +21,22 @@
 
 """ 
 当 i > 1 且 nums[i] < nums[i - 2]时，我们无法调整 nums[i - 1]，我们只能调整 nums[i] 到 nums[i - 1]。
-class Solution(object):
+
 """
-def checkPossibility(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: bool
-    """
-    N = len(nums)
-    count = 0
-    for i in range(1, N):
-        if nums[i] < nums[i - 1]:
-            count += 1
-            if i == 1 or nums[i] >= nums[i - 2]:
-                nums[i - 1] = nums[i]
-            else:
-                nums[i] = nums[i - 1]
-    return count <= 1
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        N = len(nums)
+        count = 0
+        for i in range(1, N):
+            if nums[i] < nums[i - 1]:
+                count += 1
+                if i == 1 or nums[i] >= nums[i - 2]:
+                    nums[i - 1] = nums[i]
+                else:
+                    nums[i] = nums[i - 1]
+        return count <= 1
 
---
+++ 671、二叉树中第二小的节点.py
@@ -0,0 +1,44 @@
+"""
+给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为2或0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+
+更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+
+给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+
+
+
+示例 1：
+输入：root = [2,2,5,null,null,5,7]
+输出：5
+解释：最小的值是 2 ，第二小的值是 5 。
+
+示例 2：
+输入：root = [2,2,2]
+输出：-1
+解释：最小的值是 2, 但是不存在第二小的值。
+
+
+"""
+
+
+""" 
+实际上我们通过题意可以得知，最小的元素一定的根节点，所以我们只要找到比根节点大的节点，
+直接返回就行了，更不用继续遍历当前节点下面的子节点，因为子节点的值不可能比它还小。
+"""
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        """前序遍历"""
+        def helper(root, val):
+            if not root:
+                return -1
+
+            if root.val > val:#如果是 222 的情况 那么就会子节点还是会返回-1
+                return root.val
+            left = helper(root.left, val)
+            right = helper(root.right, val)
+            if left < 0: return right
+            if right < 0: return left
+            return min(left, right)
+
+        return helper(root, root.val)
+
